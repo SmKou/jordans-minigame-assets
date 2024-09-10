e0da915c5137cbade6d47fe932138c6b4b8c4c71
SmKou <66178856+SmKou@users.noreply.github.com>
2024-09-09 23:49:36 -0700

    Adjust sample map for 18 terrains (house not included)

--- canvas/js/maps.js
+++ canvas/js/maps.js
@@ -3,23 +3,94 @@
  * Details background and objects of maps
  * Each map corresponds to route or town
  */
-const sample_1 = {
-	width: 9,
-	height: 9,
-	tiles: [],
-	objects: [],
+const sample_env = {
+	tiles: [
+		'ice',
+		'water',
+		'poison',
+		'electric',
+		'rock',
+		'ground',
+		'force',
+		'grass',
+		'dragon',
+		'fire',
+		'psychic',
+		'ghost',
+		'snow',
+		'pavement',
+		'planks',
+		'bricks',
+		'dirt',
+		'molten-rock'
+	],
+	objects: [
+		'boulder',
+		'tall-grass',
+		'step-stone',
+		'fire-rock',
+		'flower',
+		'tree',
+		'ice-rock'
+	],
 	map: [
-		'', '', '', '', '', '', '', '', '',
-		'', '', '', '', '', '', '', '', '',
-		'', '', '', '', '', '', '', '', '',
-		'', '', '', '', '', '', '', '', '',
-		'', '', '', '', '', '', '', '', '',
-		'', '', '', '', '', '', '', '', '',
-		'', '', '', '', '', '', '', '', '',
-		'', '', '', '', '', '', '', '', '',
-		'', '', '', '', '', '', '', '', '',
+		'ice', 'ice', 'ice', 'ice', 'ice', 'ice',
+		'water', 'water', 'water', 'water', 'water', 'water',
+		'ice', 'ice', 'ice', 'ice', 'ice', 'ice',
+		'water', 'water', 'water', 'water', 'water', 'water',
+		'ice', 'ice', 'ice', 'ice', 'ice', 'ice',
+		'water', 'water', 'water', 'water', 'water', 'water',
+
+		'poison', 'poison', 'poison', 'poison', 'poison', 'poison',
+		'electric', 'electric', 'electric', 'electric', 'electric', 'electric',
+		'poison', 'poison', 'poison', 'poison', 'poison', 'poison',
+		'electric', 'electric', 'electric', 'electric', 'electric', 'electric',
+		'poison', 'poison', 'poison', 'poison', 'poison', 'poison',
+		'electric', 'electric', 'electric', 'electric', 'electric', 'electric',
+
+		'rock', 'rock', 'rock', 'rock', 'rock', 'rock',
+		'ground', 'ground', 'ground', 'ground', 'ground', 'ground',
+		'rock', 'rock', 'rock', 'rock', 'rock', 'rock',
+		'ground', 'ground', 'ground', 'ground', 'ground', 'ground',
+		'rock', 'rock', 'rock', 'rock', 'rock', 'rock',
+		'ground', 'ground', 'ground', 'ground', 'ground', 'ground',
+
+		'force', 'force', 'force', 'force', 'force', 'force',
+		'grass', 'grass', 'grass', 'grass', 'grass', 'grass',
+		'force', 'force', 'force', 'force', 'force', 'force',
+		'grass', 'grass', 'grass', 'grass', 'grass', 'grass',
+		'force', 'force', 'force', 'force', 'force', 'force',
+		'grass', 'grass', 'grass', 'grass', 'grass', 'grass',
+
+		'dragon', 'dragon', 'dragon', 'dragon', 'dragon', 'dragon',
+		'fire', 'fire', 'fire', 'fire', 'fire', 'fire',
+		'dragon', 'dragon', 'dragon', 'dragon', 'dragon', 'dragon',
+		'fire', 'fire', 'fire', 'fire', 'fire', 'fire',
+		'dragon', 'dragon', 'dragon', 'dragon', 'dragon', 'dragon',
+		'fire', 'fire', 'fire', 'fire', 'fire', 'fire',
+
+		'psychic', 'psychic', 'psychic', 'psychic', 'psychic', 'psychic',
+		'ghost', 'ghost', 'ghost', 'ghost', 'ghost', 'ghost',
+		'psychic', 'psychic', 'psychic', 'psychic', 'psychic', 'psychic',
+		'ghost', 'ghost', 'ghost', 'ghost', 'ghost', 'ghost',
+		'psychic', 'psychic', 'psychic', 'psychic', 'psychic', 'psychic',
+		'ghost', 'ghost', 'ghost', 'ghost', 'ghost', 'ghost',
+
+		'snow', 'snow', 'snow', 'snow', 'snow', 'snow', 'pavement', 'pavement', 'pavement', 'pavement', 'pavement', 'pavement',
+		'snow', 'snow', 'snow', 'snow', 'snow', 'snow', 'pavement', 'pavement', 'pavement', 'pavement', 'pavement', 'pavement',
+		'snow', 'snow', 'snow', 'snow', 'snow', 'snow', 'pavement', 'pavement', 'pavement', 'pavement', 'pavement', 'pavement',
+
+		'planks', 'planks', 'planks', 'planks', 'planks', 'planks', 'bricks', 'bricks', 'bricks', 'bricks', 'bricks', 'bricks',
+		'planks', 'planks', 'planks', 'planks', 'planks', 'planks', 'bricks', 'bricks', 'bricks', 'bricks', 'bricks', 'bricks',
+		'planks', 'planks', 'planks', 'planks', 'planks', 'planks', 'bricks', 'bricks', 'bricks', 'bricks', 'bricks', 'bricks',
+
+		'dirt', 'dirt', 'dirt', 'dirt', 'dirt', 'dirt', 'molten-rock', 'molten-rock', 'molten-rock', 'molten-rock', 'molten-rock', 'molten-rock',
+		'dirt', 'dirt', 'dirt', 'dirt', 'dirt', 'dirt', 'molten-rock', 'molten-rock', 'molten-rock', 'molten-rock', 'molten-rock', 'molten-rock',
+		'dirt', 'dirt', 'dirt', 'dirt', 'dirt', 'dirt', 'molten-rock', 'molten-rock', 'molten-rock', 'molten-rock', 'molten-rock', 'molten-rock'
 	],
 	ground: {}
 }
 
-export default { sample1: sample_1 }
+// map functions
+
+export default { sample1: sample_env }
