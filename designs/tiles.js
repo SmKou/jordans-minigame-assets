a49bc416a77bc27a89d5ef1ebcdd05d9c8653878
Sm Kou <66178856+SmKou@users.noreply.github.com>
2024-09-18 13:23:10 -0700

    Add terrain refs

---
+++ designs/tiles.js
@@ -0,0 +1,41 @@
+const rock_terrain = document.getElementById("rock-terrain").getContext("2d")
+
+const cave_terrain = document.getElementById("cave-terrain").getContext("2d")
+
+const dirt_terrain = document.getElementById("dirt-terrain").getContext("2d")
+
+const grass_terrain = document.getElementById("grass-terrain").getContext("2d")
+
+const water_terrain = document.getElementById("water-terrain").getContext("2d")
+
+const deep_water_terrain = document.getElementById("deep-water-terrain").getContext("2d")
+
+const sludge_terrain = document.getElementById("sludge-terrain").getContext("2d")
+
+const lava_rock_terrain = document.getElementById("lava-rock-terrain").getContext("2d")
+
+const magma_terrain = document.getElementById("magma-terrain").getContext("2d")
+
+const ice_terrain = document.getElementById("ice-terrain").getContext("2d")
+
+const snow_terrain = document.getElementById("snow-terrain").getContext("2d")
+
+const planks_terrain_2v = document.getElementById("planks-terrain-2v").getContext("2d")
+
+const planks_terrain_2h = document.getElementById("planks-terrain-2h").getContext("2d")
+
+const planks_terrain_4v = document.getElementById("planks-terrain-4v").getContext("2d")
+
+const planks_terrain_4h = document.getElementById("planks-terrain-4h").getContext("2d")
+
+const planks_terrain_8v = document.getElementById("planks-terrain-8v").getContext("2d")
+
+const planks_terrain_8h = document.getElementById("planks-terrain-8h").getContext("2d")
+
+const bricks_terrain_1 = document.getElementById("bricks-terrain-1").getContext("2d")
+
+const bricks_terrain_2 = document.getElementById("bricks-terrain-2").getContext("2d")
+
+const bricks_terrain_4 = document.getElementById("bricks-terrain-4").getContext("2d")
+
+const pavement_terrain = document.getElementById("pavement-terrain").getContext("2d")
