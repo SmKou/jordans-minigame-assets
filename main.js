fc2cae65bf335b2465714d3fc39093a31f567141
SmKou <66178856+SmKou@users.noreply.github.com>
2024-10-05 14:40:19 -0700

    Add loader to index (remove from js)

--- main.js
+++ main.js
@@ -1,11 +1,5 @@
 import './style.css'
 
-const loader = document
-			   .createElement("div")
-			   .setAttribute("class", "loader")
-
-document.getElementById("app").append(loader)
-
 const current = {
 	area: localStorage.getItem("curr_area") || "undertown"
 }
