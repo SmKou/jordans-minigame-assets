9d5e01ba50ae309b32ca9487e62192ffb0cc3ab8
SmKou <66178856+SmKou@users.noreply.github.com>
2024-11-17 23:18:14 -0800

    Set animation prototype

---
+++ animation/main.js
@@ -0,0 +1,17 @@
+/*
+ * Animation: rolling square
+ * 32 x 32
+ *
+ * How to animate:
+ *
+ * 1. Contained canvas positioning
+ * Draw each frame to portion of canvas
+ * Move canvas with transform each frame
+ *
+ * 2. Canvas redraw
+ * Draw each frame
+ *
+ * 3. Cycle frames visibility
+ * Draw canvas for each frame
+ * Change order with z-index each frame
+ */
