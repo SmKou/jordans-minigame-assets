a34a2b31166fcdefb5cec8181b3bcfdafa16757f
SmKou <66178856+SmKou@users.noreply.github.com>
2024-11-28 12:36:11 -0800

    Add canvas and contexts for animation exp

--- animation/main.js
+++ animation/main.js
@@ -15,3 +15,16 @@
  * Draw canvas for each frame
  * Change order with z-index each frame
  */
+
+
+const cvs_exp_1 = document.getElementById("exp-1")
+const exp_1 = cvs_exp_1.getContext("2d")
+
+
+const cvs_exp_2 = document.getElementById("exp-2")
+const exp_2 = cvs_exp_2.getContext("2d")
+
+
+const cvs_exp_3 = document.querySelectorAll(".container canvas")
+const exp_3 = cvs_exp_3.map(cvs => cvs.getContext("2d"))
+
