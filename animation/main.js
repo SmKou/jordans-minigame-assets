a626a959014dec16e4dc077c53955ba7a2f2b20e
SmKou <66178856+SmKou@users.noreply.github.com>
2024-11-30 17:02:34 -0800

    Add exp 2 animation

--- animation/main.js
+++ animation/main.js
@@ -18,6 +18,7 @@
 
 const block = 64
 const unit = block / 4
+const rad = (deg) => deg * Math.PI / 180
 
 const init = (ctn) => {
 	const cvs = document.createElement("canvas")
@@ -26,8 +27,6 @@ const init = (ctn) => {
 	cvs.style.position = "absolute"
 
 	const ctx = cvs.getContext("2d")
-
-	const rad = (deg) => deg * Math.PI / 180
 
 	const frame = (deg = 0) => {
 		const cvs = new OffscreenCanvas(block, block)
@@ -66,15 +65,54 @@ const init = (ctn) => {
 
 const exp_1 = init(document.getElementById("exp-1"))
 
-let frame = 0
+let cframe = 0
 const change_frame = () => {
-	exp_1.style.left = -1 * 64 * frame + "px"
-	const delay = frame ? 300 : 900
-	frame++
-	if (frame > 3)
-		frame = 0
+	exp_1.style.left = -1 * 64 * cframe + "px"
+	const delay = cframe ? 300 : 900
+	cframe++
+	if (cframe > 3)
+		cframe = 0
 	setTimeout(change_frame, delay)
 }
 change_frame()
 
 
+const frames = (n) => {
+	const cvs = new OffscreenCanvas(block, block)
+	const ctx = cvs.getContext("2d")
+	const deg = [0, 22.5, 45, 67.5]
+	const trans = [
+		{ x: 0, y: 0 },
+		{ x: -unit / 4, y: -unit / 8},
+		{ x: -unit / 4, y: -unit / 4},
+		{ x: -unit / 4, y: -unit / 16}
+	]
+
+	ctx.translate(2 * unit, 2 * unit)
+	ctx.rotate(rad(deg[n]))
+	ctx.translate(-2 * unit, -2 * unit)
+	ctx.translate(trans[n].x, trans[n].y)
+
+	ctx.fillStyle = '#000'
+	ctx.fillRect(unit, unit, 2 * unit, 2 * unit)
+
+	return cvs
+}
+
+const exp_2 = document.getElementById("exp-2")
+exp_2.width = 64
+exp_2.height = 64
+const ctx = exp_2.getContext("2d")
+
+let dframe = 0
+const draw_frame = () => {
+	const cvs = frames(dframe)
+	ctx.clearRect(0, 0, block, block)
+	ctx.drawImage(cvs, 0, 0)
+	const delay = dframe === 0 ? 900 : 300
+	dframe++
+	if (dframe > 3)
+		dframe = 0
+	setTimeout(draw_frame, delay)
+}
+draw_frame()
