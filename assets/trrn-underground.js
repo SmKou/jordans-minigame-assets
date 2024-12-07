ccb292f7f7f0f27b58f3b3a81e5a11ab42603145
SmKou <66178856+SmKou@users.noreply.github.com>
2024-12-07 14:46:58 -0800

    WIP Add mat generation to underground.js; move tile and mat generation to fn

--- assets/trrn-underground.js
+++ assets/trrn-underground.js
@@ -1,7 +1,7 @@
 import { block, tile, half_tile, half, unit } from 'data.js'
 
 const jagged = [
-	{
+	{ // Two left-edge
 		right: () => 1,
 		down: (r) => {
 			if (r < .3) return 0
@@ -14,7 +14,6 @@ const jagged = [
 			const ctx = cvs.getContext("2d", { alpha: false })
 			if (border > 0)
 				return cvs
-
 			ctx.beginPath()
 			ctx.moveTo(half, 0)
 			ctx.lineTo(tile, half_tile)
@@ -22,8 +21,136 @@ const jagged = [
 			ctx.lineTo(tile, tile)
 			ctx.strokeStyle = '#ddd'
 			ctx.stroke()
-
+			return cvs
+		}
+	},
+	{ // Two right-edge
+		right: () => 0,
+		down: (r) => {
+			if (r < .3) return 1
+			if (r < .6) return 0
+			if (r < .8) return 3
+			return 2
+		},
+		draw(border = 0) {
+			const cvs = new OffscreenCanvas(tile, tile)
+			const ctx = cvs.getContext("2d", { alpha: false })
+			if (border < 0)
+				return cvs
+			ctx.beginPath()
+			ctx.moveTo(0, half_tile)
+			ctx.lineTo(half_tile + half, 0)
+			ctx.moveTo(0, tile)
+			ctx.lineTo(half_tile + half, half_tile)
+			ctx.strokeStyle = '#ddd'
+			ctx.stroke()
+			return cvs
+		}
+	},
+	{ // Overlapping left-edge
+		right: () => 3,
+		down: (r) => {
+			if (r < .3) return 2
+			if (r < .6) return 3
+			if (r < .8) return 0
+			return 1
+		},
+		draw(border = 0) {
+			const cvs = new OffscreenCanvas(tile, tile)
+			const ctx = cvs.getContext("2d", { alpha: false })
+			ctx.beginPath()
+			if (border >= 0) {
+				ctx.moveTo(0, half_tile)
+				ctx.lineTo(half_tile + half, 0)
+			}
+			if (border <= 0) {
+				ctx.moveTo(half, half_tile)
+				ctx.lineTo(tile, tile)
+			}
+			ctx.strokeStyle = '#ddd'
+			ctx.stroke()
+			return cvs
+		}
+	},
+	{ // Overlapping right-edge
+		right: () => 2,
+		down: (r) => {
+			if (r < .3) return 3
+			if (r < .6) return 2
+			if (r < .8) return 1
+			return 0
+		},
+		draw(border = 0) {
+			const cvs = new OffscreenCanvas(tile, tile)
+			const ctx = cvs.getContext("2d", { alpha: false })
+			ctx.beginPath()
+			if (border >= 0) {
+				ctx.moveTo(0, tile)
+				ctx.lineTo(half_tile + half, half_tile)
+			}
+			if (border <= 0) {
+				ctx.moveTo(half, 0)
+				ctx.lineTo(tile, half_tile)
+			}
+			ctx.strokeStyle = '#ddd'
+			ctx.stroke()
 			return cvs
 		}
 	}
 ]
+
+const create_container = (repeat_size) => {
+	const ctn = document.createElement("div")
+	ctn.style.marginBottom = `${tile}px`
+	ctn.style.display = 'grid'
+	ctn.style.gridTemplateColumns = `repeat(auto-fit, ${repeat_size})`
+	ctn.style.gap = `${tile}px`
+	return ctn
+}
+
+const tiles = {
+	walls: () => {
+		const ctn = create_container(tile)
+		for (let i = 0; i < jagged.length; ++i) {
+			const cvs = document.createElement("canvas")
+			cvs.width = tile
+			cvs.height = tile
+			cvs.getContext("2d").drawImage(jagged[i].draw(), 0, 0)
+			ctn.append(cvs)
+		}
+		return ctn
+	}
+}
+
+const mat_dim = (n) => block * n
+const gen_right = (tiles, idx, width = 5) => {
+	const ord = [idx]
+	while (ord.length < width) {
+		const last_idx = ord.length - 1
+		curr = tiles[ord[last_idx]].right()
+		ord.push(curr)
+	}
+	return ord
+}
+
+const gen_down = (tiles, ids, width = 5, height = 5) => {
+	while (ids.length < height)
+		ids.push(Math.floor(Math.random() * tiles.length))
+
+	const ord = []
+	for (let i = 0; i < ids.length; ++i)
+		ord.push(...gen_right(tiles, ids[i], width))
+
+	return ord
+}
+
+const gen_mat = (tiles, width, height) => {
+
+}
+
+const mats = {
+	walls: (n) => {
+		const ctn = create_container(`minmax(${mat_dim(n)}px, 1fr)`)
+		return ctn
+	}
+}
--- main.js
+++ main.js
@@ -1,140 +1,155 @@
-const block = 32
-const tile = block / 2
-const unit = tile / 8
 
-const map = {
-	width: 100,
-	height: 40,
-	edge: {
-		color: '#000',
-		points: [
-			'1-1'
-		]
-	}
+
+const select = document.getElementById("designs-select")
+const main = document.querySelector("main")
+
+const clear = () => {
+	while (main.firstChild)
+		main.removeChild(main.firstChild)
 }
 
-const cvs = document.createElement('canvas')
-cvs.width = map.width * block
-cvs.height = map.height * block
+select.addEventListener("change", e => {
+	const value = e.target.value
+})
 
-const town = (cvs) => {
-	const width = 25 * block * 2
-	const height = 20 * block * 2
-	console.log(width, height)
-	cvs.width = width
-	cvs.height = height
-	const ctx = cvs.getContext("2d")
-	ctx.fillStyle = "#000"
-	ctx.fillRect(0, 0, cvs.width, cvs.height)
-	const boundary = [
-		{ x: 1, y: 1 },
-		{ x: 6, y: 1 },
-		{ x: 6, y: 2 },
-		{ x: 7, y: 2 },
-		{ x: 7, y: 3 },
-		{ x: 8, y: 3 },
-		{ x: 8, y: 5 },
-		{ x: 7, y: 5 },
-		{ x: 7, y: 6 },
-		{ x: 6, y: 6 },
-		{ x: 6, y: 7 },
-		{ x: 8, y: 7 },
-		{ x: 8, y: 8 },
-		{ x: 9, y: 8 },
-		{ x: 9, y: 9 },
-		{ x: 12, y: 9 },
-		{ x: 12, y: 7 },
-		{ x: 13, y: 7 },
-		{ x: 13, y: 6 },
-		{ x: 15, y: 6 },
-		{ x: 15, y: 5 },
-		{ x: 17, y: 5 },
-		{ x: 17, y: 3 },
-		{ x: 18, y: 3 },
-		{ x: 18, y: 1 },
-		{ x: 24, y: 1 },
-		{ x: 24, y: 7 },
-		{ x: 22, y: 7 },
-		{ x: 22, y: 8 },
-		{ x: 19, y: 8 },
-		{ x: 19, y: 10 },
-		{ x: 18, y: 10 },
-		{ x: 18, y: 11 },
-		{ x: 15, y: 11 },
-		{ x: 15, y: 14 },
-		{ x: 17, y: 14 },
-		{ x: 17, y: 13 },
-		{ x: 18, y: 13 },
-		{ x: 18, y: 12 },
-		{ x: 20, y: 12 },
-		{ x: 20, y: 9 },
-		{ x: 21, y: 9 },
-		{ x: 21, y: 10 },
-		{ x: 22, y: 10 },
-		{ x: 22, y: 11 },
-		{ x: 24, y: 11 },
-		{ x: 24, y: 16 },
-		{ x: 23, y: 16 },
-		{ x: 23, y: 17 },
-		{ x: 21, y: 17 },
-		{ x: 21, y: 19 },
-		{ x: 14, y: 19 },
-		{ x: 14, y: 17 },
-		{ x: 13, y: 17 },
-		{ x: 13, y: 16 },
-		{ x: 12, y: 16 },
-		{ x: 12, y: 13 },
-		{ x: 10, y: 13 },
-		{ x: 10, y: 15 },
-		{ x: 9, y: 15 },
-		{ x: 9, y: 16 },
-		{ x: 8, y: 16 },
-		{ x: 8, y: 17 },
-		{ x: 6, y: 17 },
-		{ x: 6, y: 18 },
-		{ x: 5, y: 18 },
-		{ x: 5, y: 19 },
-		{ x: 1, y: 19 },
-		{ x: 1, y: 16 },
-		{ x: 2, y: 16 },
-		{ x: 2, y: 15 },
-		{ x: 3, y: 15 },
-		{ x: 3, y: 14 },
-		{ x: 5, y: 14 },
-		{ x: 5, y: 13 },
-		{ x: 7, y: 13 },
-		{ x: 7, y: 12 },
-		{ x: 4, y: 12 },
-		{ x: 4, y: 11 },
-		{ x: 2, y: 11 },
-		{ x: 2, y: 10 },
-		{ x: 1, y: 10 },
-		{ x: 1, y: 1 }
-	]
-	ctx.beginPath()
-	ctx.moveTo(boundary[0].x * block * 2, boundary[0].y * block * 2)
-	for (let i = 1; i < boundary.length; ++i)
-		ctx.lineTo(boundary[i].x * block * 2, boundary[i].y * block * 2)
-	ctx.lineWidth = 8
-	ctx.strokeStyle = "#aaa"
-	ctx.stroke()
-	ctx.fillStyle = "#fff"
-	ctx.fill()
-	ctx.closePath()
-	const color = 360 * Math.random()
-	ctx.beginPath()
-	ctx.moveTo(10 * block * 2, 9 * block * 2)
-	ctx.lineTo(10 * block * 2, 8 * block * 2)
-	ctx.lineTo(10 * block * 2 + block, 8 * block * 2)
-	ctx.lineTo(10 * block * 2 + block, 9 * block * 2)
-	ctx.strokeStyle = `hsl(${color}, 100%, 40%)`
-	ctx.stroke()
-	ctx.beginPath()
-	ctx.fillStyle = `hsl(${color}, 90%, 75%)`
-	ctx.fillRect(10 * block * 2, 8 * block * 2, block, 2 * block)
-	return cvs
-}
 
+// const block = 32
+// const tile = block / 2
+// const unit = tile / 8
+//
+// const map = {
+// 	width: 100,
+// 	height: 40,
+// 	edge: {
+// 		color: '#000',
+// 		points: [
+// 			'1-1'
+// 		]
+// 	}
+// }
+//
+// const cvs = document.createElement('canvas')
+// cvs.width = map.width * block
+// cvs.height = map.height * block
+//
+// const town = (cvs) => {
+// 	const width = 25 * block * 2
+// 	const height = 20 * block * 2
+// 	console.log(width, height)
+// 	cvs.width = width
+// 	cvs.height = height
+// 	const ctx = cvs.getContext("2d")
+// 	ctx.fillStyle = "#000"
+// 	ctx.fillRect(0, 0, cvs.width, cvs.height)
+// 	const boundary = [
+// 		{ x: 1, y: 1 },
+// 		{ x: 6, y: 1 },
+// 		{ x: 6, y: 2 },
+// 		{ x: 7, y: 2 },
+// 		{ x: 7, y: 3 },
+// 		{ x: 8, y: 3 },
+// 		{ x: 8, y: 5 },
+// 		{ x: 7, y: 5 },
+// 		{ x: 7, y: 6 },
+// 		{ x: 6, y: 6 },
+// 		{ x: 6, y: 7 },
+// 		{ x: 8, y: 7 },
+// 		{ x: 8, y: 8 },
+// 		{ x: 9, y: 8 },
+// 		{ x: 9, y: 9 },
+// 		{ x: 12, y: 9 },
+// 		{ x: 12, y: 7 },
+// 		{ x: 13, y: 7 },
+// 		{ x: 13, y: 6 },
+// 		{ x: 15, y: 6 },
+// 		{ x: 15, y: 5 },
+// 		{ x: 17, y: 5 },
+// 		{ x: 17, y: 3 },
+// 		{ x: 18, y: 3 },
+// 		{ x: 18, y: 1 },
+// 		{ x: 24, y: 1 },
+// 		{ x: 24, y: 7 },
+// 		{ x: 22, y: 7 },
+// 		{ x: 22, y: 8 },
+// 		{ x: 19, y: 8 },
+// 		{ x: 19, y: 10 },
+// 		{ x: 18, y: 10 },
+// 		{ x: 18, y: 11 },
+// 		{ x: 15, y: 11 },
+// 		{ x: 15, y: 14 },
+// 		{ x: 17, y: 14 },
+// 		{ x: 17, y: 13 },
+// 		{ x: 18, y: 13 },
+// 		{ x: 18, y: 12 },
+// 		{ x: 20, y: 12 },
+// 		{ x: 20, y: 9 },
+// 		{ x: 21, y: 9 },
+// 		{ x: 21, y: 10 },
+// 		{ x: 22, y: 10 },
+// 		{ x: 22, y: 11 },
+// 		{ x: 24, y: 11 },
+// 		{ x: 24, y: 16 },
+// 		{ x: 23, y: 16 },
+// 		{ x: 23, y: 17 },
+// 		{ x: 21, y: 17 },
+// 		{ x: 21, y: 19 },
+// 		{ x: 14, y: 19 },
+// 		{ x: 14, y: 17 },
+// 		{ x: 13, y: 17 },
+// 		{ x: 13, y: 16 },
+// 		{ x: 12, y: 16 },
+// 		{ x: 12, y: 13 },
+// 		{ x: 10, y: 13 },
+// 		{ x: 10, y: 15 },
+// 		{ x: 9, y: 15 },
+// 		{ x: 9, y: 16 },
+// 		{ x: 8, y: 16 },
+// 		{ x: 8, y: 17 },
+// 		{ x: 6, y: 17 },
+// 		{ x: 6, y: 18 },
+// 		{ x: 5, y: 18 },
+// 		{ x: 5, y: 19 },
+// 		{ x: 1, y: 19 },
+// 		{ x: 1, y: 16 },
+// 		{ x: 2, y: 16 },
+// 		{ x: 2, y: 15 },
+// 		{ x: 3, y: 15 },
+// 		{ x: 3, y: 14 },
+// 		{ x: 5, y: 14 },
+// 		{ x: 5, y: 13 },
+// 		{ x: 7, y: 13 },
+// 		{ x: 7, y: 12 },
+// 		{ x: 4, y: 12 },
+// 		{ x: 4, y: 11 },
+// 		{ x: 2, y: 11 },
+// 		{ x: 2, y: 10 },
+// 		{ x: 1, y: 10 },
+// 		{ x: 1, y: 1 }
+// 	]
+// 	ctx.beginPath()
+// 	ctx.moveTo(boundary[0].x * block * 2, boundary[0].y * block * 2)
+// 	for (let i = 1; i < boundary.length; ++i)
+// 		ctx.lineTo(boundary[i].x * block * 2, boundary[i].y * block * 2)
+// 	ctx.lineWidth = 8
+// 	ctx.strokeStyle = "#aaa"
+// 	ctx.stroke()
+// 	ctx.fillStyle = "#fff"
+// 	ctx.fill()
+// 	ctx.closePath()
+// 	const color = 360 * Math.random()
+// 	ctx.beginPath()
+// 	ctx.moveTo(10 * block * 2, 9 * block * 2)
+// 	ctx.lineTo(10 * block * 2, 8 * block * 2)
+// 	ctx.lineTo(10 * block * 2 + block, 8 * block * 2)
+// 	ctx.lineTo(10 * block * 2 + block, 9 * block * 2)
+// 	ctx.strokeStyle = `hsl(${color}, 100%, 40%)`
+// 	ctx.stroke()
+// 	ctx.beginPath()
+// 	ctx.fillStyle = `hsl(${color}, 90%, 75%)`
+// 	ctx.fillRect(10 * block * 2, 8 * block * 2, block, 2 * block)
+// 	return cvs
+// }
+//
 // const pop_map = () => {
 // 	const app = document.getElementById("app")
 // 	const loader = document.querySelector(".loader")
