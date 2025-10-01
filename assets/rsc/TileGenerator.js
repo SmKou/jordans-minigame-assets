bf581efd439ae93c24190a2e43c0c3f897f5d39b
SmKou <66178856+SmKou@users.noreply.github.com>
2025-09-30 22:02:36 -0700

    Add generation methods

--- assets/rsc/TileGenerator.js
+++ assets/rsc/TileGenerator.js
@@ -1,3 +1,10 @@
+import canvas, { sizes } from "./canvas"
+
+const { block, tile, half_tile } = sizes
+
+export const TILE_SIDE = block
+export const SQUARE_SIDE = 2 * block
+
 export const gen_cvs = (side = block) => {
 	const cvs = new OffscreenCanvas(side, side)
 	const ctx = cvs.getContext("2d")
@@ -5,18 +12,19 @@ export const gen_cvs = (side = block) => {
 		cvs,
 		ctx,
 		end() {
-			ctx.strokeStyle = "#000"
+			ctx.lineWidth = 0.5
+			ctx.strokeStyle = "#111"
 			ctx.stroke()
 		}
 	}
 }
 
 export const gen_square = (square, tiles) => () => {
-	const tile_names = Object.keys(square.tiles)
-	const { cvs, ctx, end } = gen_cvs(SQUARE_SIDE)
+	const tile_names = Object.keys(square)
+	const { cvs, ctx } = gen_cvs(SQUARE_SIDE)
 	tile_names.forEach(tile_name => {
 		const tile = tiles[tile_name].draw()
-		const placements = square.tiles[tile_name]
+		const placements = square[tile_name]
 		placements.forEach(place => {
 			switch (place) {
 				case -1:
@@ -33,14 +41,13 @@ export const gen_square = (square, tiles) => () => {
 			}
 		})
 	})
-	end()
 	return cvs
 }
 
-const gen_border = (tile, side = block, line_width = 0.5) {
+const gen_border = (tile, side = block, line_width = 0.5) => {
 	const cvs = tile(side)
 	const ctx = cvs.getContext("2d")
-	ctx.strokeStyle = "#000"
+	ctx.strokeStyle = "#111"
 	ctx.lineWidth = line_width
 	ctx.strokeRect(0, 0, side, side)
 	return cvs
@@ -61,37 +68,78 @@ export const gen_sample = (data, side, bordered = false) => {
 		: data.draw()
 	ctx.drawImage(tile, 0, 0)
 	container.append(cvs)
-	return bordered
-		? container
-		: [container, gen_sample(data, side, true)]
+	return container
 }
 
-export const gen_terrain = (data, side, width, height) => {
+export const gen_terrain = (data, side, width, height, bordered = false) => {
 	const container = canvas.container(
 		data.caption,
-		{
-			width: side * width,
-			height: side * height,
-			margin: tile + "px"
-		}
+		{ width: side * width, height: side * height, margin: tile + "px" }
 	)
 	const cvs = document.createElement("canvas")
 	cvs.width = side * width
 	cvs.height = side * height
 	const ctx = cvs.getContext("2d")
-	for (let xy = 0; xy < width * height; ++xy) {
-		const tile = data.draw()
-		const x = (xy % width) * side
-		const y = Math.floor(xy / height) * side
-		ctx.drawImage(tile, x, y)
-	}
+	const drawn_tile = bordered
+		? gen_border(data.draw)
+		: data.draw()
+	for (let y = 0; y < height; ++y)
+		for (let x = 0; x < width; ++x)
+			ctx.drawImage(drawn_tile, x * side, y * side)
 	container.append(cvs)
 	return container
 }
 
+const tile_demos = (tiles, grids, width, height, add_borders = false) => {
+	const [samples, terrains] = grids
+	const tile_names = Object.keys(tiles)
+	tile_names.forEach(tile_name => {
+		const unbordered = gen_sample(tiles[tile_name], TILE_SIDE)
+		samples.append(unbordered)
+		if (add_borders) {
+			const bordered = gen_sample(tiles[tile_name], TILE_SIDE, true)
+			samples.append(bordered)
+		}
+	})
+	tile_names.forEach(tile_name => {
+		const unbordered = gen_terrain(tiles[tile_name], TILE_SIDE, width, height)
+		terrains.append(unbordered)
+		if (add_borders) {
+			const bordered = gen_terrain(tiles[tile_name], TILE_SIDE, width, height, true)
+			terrains.append(bordered)
+		}
+	})
+	return grids
+}
+
+const square_demos = (squares, tiles, grids, width, height) => {
+	const [samples, terrains] = grids
+	const square_names = Object.keys(squares)
+	square_names.forEach(square_name => {
+		const caption = squares[square_name].caption
+		const draw = gen_square(squares[square_name].tiles, tiles)
+		const sample = gen_sample({ caption, draw }, SQUARE_SIDE)
+		samples.append(sample)
+	})
+	square_names.forEach(square_name => {
+		const caption = squares[square_name].caption
+		const draw = gen_square(squares[square_name].tiles, tiles)
+		const terrain = gen_terrain({ caption, draw }, SQUARE_SIDE, width, height)
+		terrains.append(terrain)
+	})
+	return grids
+}
+
+export const demo = {
+	cvs: gen_cvs,
+	tiles: tile_demos,
+	TILE: TILE_SIDE,
+	squares: square_demos
+}
+
 export default {
 	cvs: gen_cvs,
-	squ: gen_square,
-	spl: gen_sample,
-	trrn: gen_terrain
+	square: gen_square,
+	sample: gen_sample,
+	terrain: gen_terrain
 }
